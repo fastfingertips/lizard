cdc72f973e26c56e2b041b125967b408811b849e
FastFingertips <fastfingertips@gmail.com>
2025-03-07 14:18:26 +0300

    refactor(main): simplify input handling with st.stop

--- main.py
+++ main.py
@@ -18,12 +18,6 @@ from models.utils import (
     get_csv_syntax
     )
 
-def check_user_input(user_input_data):
-    if not user_input_data:
-        st.write('_Awaiting input.._')
-        return False
-    return True
-
 def catch_error_message(url_dom) -> bool|str:
     err = url_dom.find('body', class_='error')
     if err:
@@ -48,80 +42,83 @@ if __name__ == "__main__":
     print('Input initialized.')
 
     # Process data
-    if check_user_input(user_input_data):
-        input_is_url = is_url(user_input_data)
-        url_is_short = is_short_url(user_input_data)
+    if not user_input_data:
+        st.write('_Awaiting input.._')
+        st.stop()
+    
+    input_is_url = is_url(user_input_data)
+    url_is_short = is_short_url(user_input_data)
 
-        if url_is_short:
-            user_input = user_input_data.replace('/detail', '')
-        else:
-            user_input = convert_to_pattern(user_input_data)
-            user_input = input_manager.convert_to_url(user_input)
+    if url_is_short:
+        user_input = user_input_data.replace('/detail', '')
+    else:
+        user_input = convert_to_pattern(user_input_data)
+        user_input = input_manager.convert_to_url(user_input)
 
-        if not user_input:
-            st.warning('**username/list-title.**', icon='💡')
-        else:
-            # create checker object for page
-            url_dom = get_dom_from_url(user_input)
-            err_msg = catch_error_message(url_dom)
+    if not user_input:
+        st.warning('**username/list-title.**', icon='💡')
+    else:
+        # create checker object for page
+        url_dom = get_dom_from_url(user_input)
+        err_msg = catch_error_message(url_dom)
 
-            checker = Checker(url_dom)
-            list_meta_verify = checker.check_page_is_list()
+        checker = Checker(url_dom)
+        list_meta_verify = checker.check_page_is_list()
 
-            if err_msg:
-                st.error(f'{err_msg}', icon='👀')
-            if list_meta_verify['is_list']:
-                button = st.button('Get again.')
+        if err_msg:
+            st.error(f'{err_msg}', icon='👀')
+        if list_meta_verify['is_list']:
+            button = st.button('Get again.')
 
-                # Notifier
-                notifier = Notifier()
-                notifier.set_link_code("fastfingertips-lizard")
-                notifier.send(f'List verified: {user_input}')
+            # Notifier
+            notifier = Notifier()
+            notifier.set_link_code("fastfingertips-lizard")
+            notifier.send(f'List verified: {user_input}')
 
-                # create checker object for list
-                checked_list = checker.user_list_check(user_input)
+            # create checker object for list
+            checked_list = checker.user_list_check(user_input)
 
-                # address is list, so we can create the object now
-                movie_list = MovieList(
-                    Url(
-                        checked_list['list_url'],
-                        url_dom
-                    )
+            # address is list, so we can create the object now
+            movie_list = MovieList(
+                Url(
+                    checked_list['list_url'],
+                    url_dom
                 )
+            )
 
-                list_details = {}
-                for key, value in movie_list.__dict__.items():
-                    list_details[key] = '🚫' if 'dom' in key else value
-                json_info = st.json(list_details, expanded=False)
-                # since this process may take a long time, we print the list information
-                # ... on the screen before. this way we can see which list is downloaded.
+            list_details = {}
+            for key, value in movie_list.__dict__.items():
+                list_details[key] = '🚫' if 'dom' in key else value
+            json_info = st.json(list_details, expanded=False)
+            # since this process may take a long time, we print the list information
+            # ... on the screen before. this way we can see which list is downloaded.
 
-                notifier.send(f'List parsing: {user_input}')
-                if checked_list['list_avaliable']:
-                    st.dataframe(
-                        pd.DataFrame(
-                            movie_list.movies,
-                            columns=["Rank", "Year", "Title", "LetterboxdURI"]
-                        ),
-                        hide_index=True,
-                        use_container_width=True,
+            notifier.send(f'List parsing: {user_input}')
+            if checked_list['list_avaliable']:
+                st.dataframe(
+                    pd.DataFrame(
+                        movie_list.movies,
+                        columns=["Rank", "Year", "Title", "LetterboxdURI"]
+                    ),
+                    hide_index=True,
+                    use_container_width=True,
+                )
+                notifier.send(f'List parsed: {user_input}')
+
+                if False:
+                    # Download process
+                    csv_data = get_csv_syntax(movie_list.movies)
+                    download_filename = movie_list.slug + '.csv'
+
+                    download_button = st.download_button(
+                        label="Download CSV",
+                        data=csv_data,
+                        file_name=download_filename,
+                        mime='text/csv'
                     )
-                    notifier.send(f'List parsed: {user_input}')
 
-                    if False:
-                        # Download process
-                        csv_data = get_csv_syntax(movie_list.movies)
-                        download_filename = movie_list.slug + '.csv'
-
-                        download_button = st.download_button(
-                            label="Download CSV",
-                            data=csv_data,
-                            file_name=download_filename,
-                            mime='text/csv'
-                        )
-
-                        if download_button:
-                            st.success(f'{download_filename} downloaded.')
-                            notifier.send(f'List downlaoded: {user_input}')
-            else:
-                st.warning('Please enter a valid **list url** or **username/list-title.**', icon='💡')
+                    if download_button:
+                        st.success(f'{download_filename} downloaded.')
+                        notifier.send(f'List downlaoded: {user_input}')
+        else:
+            st.warning('Please enter a valid **list url** or **username/list-title.**', icon='💡')
