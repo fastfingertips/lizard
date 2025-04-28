65d0d978ebc75b1e647321e86c30cbe97a8cf00c
FastFingertips <fastfingertips@gmail.com>
2025-04-28 18:28:28 +0300

    feat(url): reorganize URL handling into utils and __init__
    - Move URL helper functions to utils module
    - Define URL class in __init__

---
+++ models/url/__init__.py
@@ -0,0 +1,45 @@
+from models.parser import DomParser
+from models.utils import get_dom_from_url
+from models.url.utils import get_list_slug
+
+class Url():
+
+    def __init__(self, url, url_dom=None):
+        
+        self.url = url
+        self.detail_url = url + 'detail/'
+        self.page_url = self.detail_url + 'page/'
+
+        self._url_dom = url_dom
+        self._detail_url_dom = None
+
+        self.slug = get_list_slug(self.url)
+
+        self._dom_parser = None
+        self._detail_dom_parser = None
+
+    @property
+    def url_dom(self):
+        if not self._url_dom:
+            self._url_dom = get_dom_from_url(self.url)
+            self._dom_parser = DomParser(self.url_dom)
+        return self._url_dom
+    
+    @property
+    def detail_url_dom(self):
+        if not self._detail_url_dom:
+            self._detail_url_dom = get_dom_from_url(self.detail_url)
+            self._detail_dom_parser = DomParser(self._detail_url_dom)
+        return self._detail_url_dom
+    
+    @property
+    def dom_parser(self):
+        if not self._dom_parser:
+            self._dom_parser = DomParser(self.url_dom)
+        return self._dom_parser
+    
+    @property
+    def detail_dom_parser(self):
+        if not self._detail_dom_parser:
+            self._detail_dom_parser = DomParser(self.detail_url_dom)
+        return self._detail_dom_parser
--- models/url/utils.py
+++ models/url/utils.py
@@ -1,55 +1,5 @@
 import validators
-from models.parser import DomParser
-from models.utils import get_dom_from_url
-from models.constants import (
-    URL_PROTOCOLS,
-    DOMAIN_SHORT,
-    DOMAIN_MATCHES,
-)
-
-
-class Url():
-
-    def __init__(self, url, url_dom=None):
-        
-        self.url = url
-        self.detail_url = url + 'detail/'
-        self.page_url = self.detail_url + 'page/'
-
-        self._url_dom = url_dom
-        self._detail_url_dom = None
-
-        self.slug = get_list_slug(self.url)
-
-        self._dom_parser = None
-        self._detail_dom_parser = None
-
-    @property
-    def url_dom(self):
-        if not self._url_dom:
-            self._url_dom = get_dom_from_url(self.url)
-            self._dom_parser = DomParser(self.url_dom)
-        return self._url_dom
-    
-    @property
-    def detail_url_dom(self):
-        if not self._detail_url_dom:
-            self._detail_url_dom = get_dom_from_url(self.detail_url)
-            self._detail_dom_parser = DomParser(self._detail_url_dom)
-        return self._detail_url_dom
-    
-    @property
-    def dom_parser(self):
-        if not self._dom_parser:
-            self._dom_parser = DomParser(self.url_dom)
-        return self._dom_parser
-    
-    @property
-    def detail_dom_parser(self):
-        if not self._detail_dom_parser:
-            self._detail_dom_parser = DomParser(self.detail_url_dom)
-        return self._detail_dom_parser
-
+from models.constants import URL_PROTOCOLS, DOMAIN_SHORT, DOMAIN_MATCHES
 
 def check_url_match(base_url, target_url) -> bool:
     """
