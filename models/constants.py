8ba4fa1b416d1238e6151f643dd9039b01736c0a
FastFingertips <fastfingertips@gmail.com>
2025-05-26 06:23:26 +0300

    feat(constants): add full and short domain URLs

--- models/constants.py
+++ models/constants.py
@@ -1,5 +1,9 @@
 URL_PROTOCOLS = ['http://', 'https://']
+
 DOMAIN_FULL = 'letterboxd.com'
 DOMAIN_SHORT = 'boxd.it'
+
 SITE = 'https://letterboxd.com/'
+SITE_SHORT = 'https://boxd.it/'
+
 DOMAIN_MATCHES = ['letterboxd.com/', 'boxd.it/']
