2d6bcd98fd5888f15cb1c2af86e0d65a672b6b84
FastFingertips <fastfingertips@gmail.com>
2025-07-27 23:24:02 +0300

    refactor: extract movie transformation logic to a shared function

--- models/movie_list.py
+++ models/movie_list.py
@@ -2,25 +2,16 @@
 from letterboxdpy.pages.user_list import extract_movies
 from letterboxdpy.pages import user_list, user_watchlist
 
-
 class WatchList:
+
     def __init__(self, url, username: str):
         self.url = url.url
         self.username = username
 
     def get_movies(self) -> list:
-        """Get movies from watchlist"""
+        """Get movies from watchlist."""
         letterboxd_movies = extract_movies(self.url, items_per_page=user_watchlist.UserWatchlist.FILMS_PER_PAGE)
-
-        movies = []
-        for rank, (_, movie_data) in enumerate(letterboxd_movies.items(), 1):
-            movie = {
-                "Rank": rank,
-                "Title": movie_data.get('name', ''),
-                "LetterboxdURI": movie_data.get('url', '')
-            }
-            movies.append(movie)
-        return movies
+        return _transform_to_ranked_movies(letterboxd_movies)
 
     @property
     def movies(self):
@@ -28,25 +19,29 @@ class WatchList:
 
 
 class UserList:
+
     def __init__(self, url, username: str, slug: str):
         self.url = url.url
         self.username = username
         self.slug = slug
 
     def get_movies(self) -> list:
-        """Get movies from user list"""
+        """Get movies from user list."""
         letterboxd_movies = extract_movies(self.url, items_per_page=user_list.UserList.LIST_ITEMS_PER_PAGE)
-
-        movies = []
-        for rank, (_, movie_data) in enumerate(letterboxd_movies.items(), 1):
-            movie = {
-                "Rank": rank,
-                "Title": movie_data.get('name', ''),
-                "LetterboxdURI": movie_data.get('url', '')
-            }
-            movies.append(movie)
-        return movies
+        return _transform_to_ranked_movies(letterboxd_movies)
 
     @property
     def movies(self):
         return self.get_movies()
+
+def _transform_to_ranked_movies(letterboxd_movies: dict) -> list:
+    """Transform letterboxd movies to ranked list."""
+    movies = []
+    for rank, (_, movie_data) in enumerate(letterboxd_movies.items(), 1):
+        movie = {
+            "Rank": rank,
+            "Title": movie_data.get('name', ''),
+            "LetterboxdURI": movie_data.get('url', '')
+        }
+        movies.append(movie)
+    return movies
