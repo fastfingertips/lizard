be93148532e90126a92a1d45667aa87b6e769161
FastFingertips <fastfingertips@gmail.com>
2025-07-29 03:38:11 +0300

    refactor: separate WatchList and UserList into dedicated modules

---
+++ models/userlist.py
@@ -0,0 +1,61 @@
+from letterboxdpy.pages.user_list import extract_movies
+from letterboxdpy.pages import user_list
+from utils.transformers import transform_to_ranked_movies
+
+
+class UserList(user_list.UserList):
+    """
+    UserList class for handling Letterboxd user lists.
+    Inherits from letterboxdpy's UserList for core functionality.
+    """
+
+    def __init__(self, url, username: str, slug: str):
+        """
+        Initialize UserList with URL, username, and slug.
+        
+        Args:
+            url: URL object containing the list URL
+            username: Letterboxd username
+            slug: List slug identifier
+        """
+        super().__init__(username, slug)
+        self.url = url.url
+        self.username = username
+        self.slug = slug
+
+    def get_movies(self) -> list:
+        """
+        Get movies from user list.
+        
+        Returns:
+            List of movies in ranked format
+        """
+        letterboxd_movies = extract_movies(self.url, items_per_page=self.LIST_ITEMS_PER_PAGE)
+        return transform_to_ranked_movies(letterboxd_movies)
+
+    @property
+    def movies(self):
+        """Property to get movies from user list."""
+        return self.get_movies()
+    
+    @property
+    def metadata(self):
+        """
+        Get list metadata as dictionary.
+        
+        Returns:
+            Dictionary containing list metadata
+        """
+        return {
+            'url': self.url,
+            'username': self.username,
+            'slug': self.slug,
+            'title': super().get_title(),
+            'author': super().get_author(),
+            'count': super().get_count(),
+            'description': super().get_description(),
+            'date_created': super().get_date_created(),
+            'date_updated': super().get_date_updated(),
+            'tags': super().get_tags(),
+            'available': True
+        }
---
+++ models/watchlist.py
@@ -0,0 +1,53 @@
+from letterboxdpy.pages.user_list import extract_movies
+from letterboxdpy.pages import user_watchlist
+from utils.transformers import transform_to_ranked_movies
+
+
+class WatchList(user_watchlist.UserWatchlist):
+    """
+    WatchList class for handling Letterboxd user watchlists.
+    Inherits from letterboxdpy's UserWatchlist for core functionality.
+    """
+
+    def __init__(self, url, username: str):
+        """
+        Initialize WatchList with URL and username.
+        
+        Args:
+            url: URL object containing the watchlist URL
+            username: Letterboxd username
+        """
+        super().__init__(username)
+        self.url = url.url
+        self.username = username
+
+    def get_movies(self) -> list:
+        """
+        Get movies from watchlist.
+        
+        Returns:
+            List of movies in ranked format
+        """
+        letterboxd_movies = extract_movies(self.url, items_per_page=self.FILMS_PER_PAGE)
+        return transform_to_ranked_movies(letterboxd_movies)
+
+    @property
+    def movies(self):
+        """Property to get movies from watchlist."""
+        return self.get_movies()
+    
+    @property
+    def metadata(self):
+        """
+        Get watchlist metadata as dictionary.
+        
+        Returns:
+            Dictionary containing watchlist metadata
+        """
+        return {
+            'url': self.url,
+            'username': self.username,
+            'count': super().get_count(),
+            'owner': super().get_owner(),
+            'available': True
+        }
