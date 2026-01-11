82442b9af82e7eabb96f3de32e8e39ab8230e4c9
FastFingertips <fastfingertips@gmail.com>
2026-01-11 04:47:00 +0300

    refactor: replace local constants and utils with letterboxdpy imports

--- constants.py
+++ constants.py
@@ -1,45 +1,9 @@
-# Column definitions for different data types
-LIST_COLUMNS = ['Rank', 'Title', 'LetterboxdURI']
-WATCHLIST_COLUMNS = ['Rank', 'Title', 'LetterboxdURI']
-
-# Letterboxd official import format column mappings
-# https://letterboxd.com/about/importing-data/
-LETTERBOXD_COLUMNS = {
-    'letterboxd_uri': 'LetterboxdURI',
-    'tmdb_id': 'tmdbID',
-    'imdb_id': 'imdbID',
-    'title': 'Title',
-    'year': 'Year',
-    'directors': 'Directors',
-    'rating': 'Rating',
-    'rating10': 'Rating10',
-    'watched_date': 'WatchedDate',
-    'rewatch': 'Rewatch',
-    'tags': 'Tags',
-    'review': 'Review',
-    'rank': 'Rank'
-}
-
-# Required columns - at least one must be present
-REQUIRED_COLUMNS = ['LetterboxdURI', 'tmdbID', 'imdbID', 'Title']
-
-# Basic validation rules
-RATING_RANGE = (0.5, 5.0)  # Rating out of 5
-RATING10_RANGE = (1, 10)   # Rating out of 10
-DATE_FORMAT = 'YYYY-MM-DD'  # WatchedDate format
-FILE_SIZE_LIMIT = '1MB'     # Maximum file size
-
-# CSV export format column mappings
-# https://letterboxd.com/about/importing-data/
-CSV_FORMAT_COLUMNS = {
-    "Letterboxd": {
-        # Official Letterboxd import format
-        "Title": "Title",
-        "LetterboxdURI": "LetterboxdURI"
-    },
-    "TMDB": {
-        # TMDB compatible format (used by import tools)
-        "Title": "Name",
-        "LetterboxdURI": "Letterboxd URI"
-    }
-}
+from letterboxdpy.constants.project import (
+    LIST_COLUMNS,
+    WATCHLIST_COLUMNS,
+    LETTERBOXD_COLUMNS,
+    CSV_FORMAT_COLUMNS,
+    REQUIRED_COLUMNS,
+    RATING_RANGE,
+    RATING10_RANGE
+)
--- utils/data.py
+++ utils/data.py
@@ -1,29 +1,4 @@
-from constants import LETTERBOXD_COLUMNS
-
-
-def create_movie_data(**kwargs):
-    """
-    Create movie data dictionary following Letterboxd import format.
-    
-    Supports all official Letterboxd CSV import fields:
-    https://letterboxd.com/about/importing-data/
-    
-    Args:
-        **kwargs: Movie data fields (letterboxd_uri, title, year, rating, etc.)
-        
-    Returns:
-        dict: Filtered movie data with Letterboxd column names
-    """
-    
-    def _is_valid_value(value):
-        """Check if value is valid for Letterboxd import."""
-        return value and isinstance(value, (str, int, bool))
-    
-    # Filter and map valid fields only
-    movie_data = {}
-    for field_name, value in kwargs.items():
-        if field_name in LETTERBOXD_COLUMNS and _is_valid_value(value):
-            column_name = LETTERBOXD_COLUMNS[field_name]
-            movie_data[column_name] = value
-            
-    return movie_data
+from letterboxdpy.utils.utils_csv import (
+    create_movie_data,
+    transform_to_ranked_movies
+)
